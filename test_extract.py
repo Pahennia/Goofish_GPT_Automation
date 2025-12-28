fdbaa75bf20067cb2d7ed44098dbe135cf87d743
Pahennia <3561898074kk@gmail.com>
2025-12-28 03:38:38 -0800

    grab all images and avoid clonned image

--- test_extract.py
+++ test_extract.py
@@ -1,13 +1,12 @@
 from playwright.sync_api import sync_playwright
 import requests
 
-# ⚠️ 换成你自己的商品页面完整 URL
-URL = "https://www.goofish.com/item?spm=a21ybx.search.searchFeedList.2.1d3d41dbIy2gK1&id=848180106380&categoryId=126860474"
+#定位网站URL
+URL = "https://www.goofish.com/item?spm=a21ybx.search.searchFeedList.2.1d3d41db5sx6bc&id=1007751981044&categoryId=126860474"
 
 def GetImageURL(page, index):
     #等待目标出现
     page.wait_for_selector(f'.slick-slide[data-index="{index}"] img')
-
     #取图片URL
     img = page.query_selector(f'.slick-slide[data-index="{index}"] img')
     return img.get_attribute("src")
@@ -23,12 +22,12 @@ def main():
 
         #获取图片 URL
         img_url = []
-        for i in range(5):
+        for i in range(15):
             img_url.append(GetImageURL(page,i))
 
 
 
-        #假装是用户
+        #浏览器合法化请求头
         headers = {
             "User-Agent": (
                 "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
@@ -39,7 +38,17 @@ def main():
         }
 
         #下载图片
-        for n in range(5):
+        for n in range(15):
+            #定义图片
+            img = page.query_selector(f'.slick-slide[data-index="{n}"] img')
+            #定位图片的上层分类元素
+            slide = img.evaluate_handle("el => el.closest('.slick-slide')")
+            #获取class属性
+            class_attr = slide.get_attribute("class") or ""
+            #判断有没有clone标记
+            if "slick-cloned" in class_attr:
+                #若被标记，跳过下载
+                break
             #处理相对URL
             if img_url[n].startswith("//"):
                 img_url[n] = "https:" + img_url[n]
