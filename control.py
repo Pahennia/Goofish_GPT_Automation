4e003aa45fe0e1a361fb468d4639f23dab220c4e
Pahennia <3561898074kk@gmail.com>
2025-12-31 16:43:08 -0800

    The code structure and logic have been optimized, and it can now collect all the links from 40 pages at once. Although it's still single-threaded, the speed has been slightly improved.

--- control.py
+++ control.py
@@ -5,10 +5,16 @@ import product_URL
 test_page_URL = "https://www.goofish.com/search?q=%E5%B7%B4%E9%BB%8E%E4%B8%96%E5%AE%B6Track1.0&spm=a21ybx.search.searchInput.0"
 
 def main():
-    current_page_URLs = product_URL.get_product_URL(test_page_URL)
-    for url in current_page_URLs:
-        image_download.download_image(url)
-
-
+    all_urls = []
+    all_urls = product_URL.get_product_URL(test_page_URL)
+    # for url in all_urls:
+    #     image_download.download_image(url)
+    print(all_urls)
+    
+    with open("urls.txt", "w", encoding="utf-8") as f:
+        for url in all_urls:
+            f.write(url + "\n")
+            
 if __name__ == "__main__":
     main()
+
--- product_URL.py
+++ product_URL.py
@@ -1,14 +1,28 @@
 from playwright.sync_api import sync_playwright
+from playwright.sync_api import sync_playwright, TimeoutError
 import requests
 
-Test_Home_URL = "https://www.goofish.com/search?spm=a21ybx.home.searchHistory.1.6d833da6XHiYYZ&q=%E5%B7%B4%E9%BB%8E%E4%B8%96%E5%AE%B6Track1.0"
-def get_product_URL(Home_URL):
-    with sync_playwright() as p:
-        #启动浏览器，初始化引擎
-        browser = p.chromium.launch(headless=False)
-        page = browser.new_page()
-        page.goto(Home_URL)
-        page.wait_for_function(
+
+Test_Home_URL = "https://www.goofish.com/search?q=%E5%B7%B4%E9%BB%8E%E4%B8%96%E5%AE%B6track1.0&spm=a21ybx.search.searchInput.0"
+
+def login_intercept_check(page):
+    try:
+        page.wait_for_selector(
+            'iframe[id*="xy-login-iframe"]',
+            timeout=1000
+        )
+        print("检测到登录拦截，刷新页面")
+        page.reload(wait_until="domcontentloaded")
+    except TimeoutError:
+        print("未检测到登录拦截")
+
+def flip_page(page, page_num):
+    if page_num <= 49:
+        page.click('button:has(div[class*="arrow-right"])')
+        print(f"已翻至第 {page_num+1} 页")
+
+def wait_for_page_load(page):
+    page.wait_for_function(
             """
             () => {
                const container = document.querySelector(
@@ -23,30 +37,52 @@ def get_product_URL(Home_URL):
             """,
            timeout=20000
         )
+    print("页面加载完成")
+    
+
+def get_product_URL(Home_URL):
+    all_urls = []
+    with sync_playwright() as p:
+        #启动浏览器，初始化引擎
+        browser = p.chromium.launch(headless=False)
+        page = browser.new_page()
+        page.goto(Home_URL, wait_until="domcontentloaded")
+        wait_for_page_load(page)
 
         #浏览器合法化请求头
-        headers = {
+        page.set_extra_http_headers({
             "User-Agent": (
                 "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
                 "AppleWebKit/537.36 (KHTML, like Gecko) "
                 "Chrome/120.0.0.0 Safari/537.36"
             ),
-            "Referer": Home_URL,
-        }
-        #获取所有商品 URL
+            "Referer": Home_URL
+        })
+        #检测登录拦截
+        login_intercept_check(page)
+        #定位URL容器
         container = page.query_selector('div[data-spm="searchFeedList"]')
         links = container.query_selector_all(
             'a[href^="https://www.goofish.com/item"]'
+        
         )
-        #赋值URL列表
-        urls = []
-        for a in links:
-            href = a.get_attribute("href")
-            urls.append(href)
+        #为每一页循环获取URL
+        for i in range(1, 39):
+            #加载和初始化
+            login_intercept_check(page)
+            wait_for_page_load(page)
+            #获取链接
+            for a in links:
+                href = a.get_attribute("href")
+                all_urls.append(href)
+            print(f"第 {i} 页 URL 获取完成")
+            #页面翻页，初始化
+            flip_page(page, i)
+            
 
-
-        browser.close()
-    return urls
+        
+        
+    return all_urls
 
 if __name__ == "__main__":
-    print(get_product_URL(Test_Home_URL))
+    get_product_URL(Test_Home_URL)
